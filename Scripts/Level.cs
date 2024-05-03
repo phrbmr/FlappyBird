500150d14ebac005a4a400b8e2ca54cb67e49b5a
phrbmr <phrbmr@hotmail.com>
2024-05-03 14:31:10 -0300

    Update Level.cs

--- Scripts/Level.cs
+++ Scripts/Level.cs
@@ -6,20 +6,70 @@ public class Level : MonoBehaviour
 {
     private const float CAMERA_ORTHO_SIZE = 50f;
     private const float PIPE_WIDTH = 7.8f;
-    private const float PIPE_HEAD_HEIGHT = 3.75f; 
+    private const float PIPE_HEAD_HEIGHT = 3.75f;
+    private const float PIPE_MOVE_SPEED = 3f;
+    private const float PIPE_DESTROY_X_POSITION = -100f;
+
+    private List<Pipe> pipeList;
+
+    private void Awake() {
+        pipeList = new List<Pipe>();
+    }
 
     private void Start() {
-        CreatePipe(50f, 0f);
-        CreatePipe(50f, 20f);
+        CreateGapPipes(50f, 20f, 20f);
     }
-    private void CreatePipe(float height, float xPosition) {
+
+    private void Update() {
+        HandlePipeMovement();
+    }
+
+    private class Pipe {
+
+        private Transform pipeHeadTransform;
+        private Transform pipeBodyTransform;
+
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform) {
+            this.pipeHeadTransform = pipeHeadTransform;
+            this.pipeBodyTransform = pipeBodyTransform;
+        }
+
+        public void Move() {
+            pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+
+        public float GetXPosition() {
+            return pipeHeadTransform.position.x;
+        }
+
+        public void DestroySelf() {
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
+        }
+    }
+
+    private void CreatePipe(float height, float xPosition, bool createBottom) {
         // Pipe Head setup
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
-        pipeHead.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE + height - (PIPE_HEAD_HEIGHT* 0.5f));
-        
+        float pipeHeadYPosition;
+        if (createBottom) {
+            pipeHeadYPosition = -CAMERA_ORTHO_SIZE + height - (PIPE_HEAD_HEIGHT * 0.5f);
+        } else {
+            pipeHeadYPosition = +CAMERA_ORTHO_SIZE - height + (PIPE_HEAD_HEIGHT * 0.5f);
+        }
+        pipeHead.position = new Vector3(xPosition, pipeHeadYPosition);
+
         // Pipe Body positioning
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
-        pipeBody.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE);
+        float pipeBodyYPosition;
+        if (createBottom) {
+            pipeBodyYPosition = -CAMERA_ORTHO_SIZE;
+        } else {
+            pipeBodyYPosition = +CAMERA_ORTHO_SIZE;
+            pipeBody.localScale = new Vector3(1, -1, 1);
+        }
+        pipeBody.position = new Vector3(xPosition, pipeBodyYPosition);
 
         SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySpriteRenderer.size = new Vector2(PIPE_WIDTH, height);
@@ -27,5 +77,24 @@ public class Level : MonoBehaviour
         BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
         pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * 0.5f);
+
+        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        pipeList.Add(pipe);
+    }
+
+    private void CreateGapPipes(float gapY, float gapSize, float xPosition) {
+        CreatePipe(gapY - gapSize * .5f, xPosition, true);
+        CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * .5f, xPosition, false);
+    }
+
+    private void HandlePipeMovement() {
+        foreach (Pipe pipe in pipeList) {
+            pipe.Move();
+            if (pipe.GetXPosition() < PIPE_DESTROY_X_POSITION) {
+                pipe.DestroySelf();
+            }
+        }
+        
     }
 }
+
